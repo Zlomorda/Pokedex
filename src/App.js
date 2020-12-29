42875d6c1a81516f5e851c008af41349359264f4
Zlomorda <61455902+Zlomorda@users.noreply.github.com>
2020-12-29 06:11:00 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Pokegame from "./Pokegame";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pokegame />
+      </div>
+    );
+  }
+}
+
+export default App;
